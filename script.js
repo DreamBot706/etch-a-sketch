9730dcd75e4eb4ad8becaefdeb11124584969de0
DreamBot706 <187842058+DreamBot706@users.noreply.github.com>
2025-01-06 07:56:35 +0000

    Add Shading, Rainbow mode and Display Slider Added the shading and rainbow mode, also added the color picker for the color and shading mode, and finally made a display slider for changing size of grid from 1x1 to 100x100

--- script.js
+++ script.js
@@ -3,11 +3,23 @@ const grid_container = document.querySelector(".container")
 const rainbow_button = document.querySelector("button.orange")
 const clear_button = document.querySelector("button.green")
 const shading_button = document.querySelector("button.red")
+const color_button = document.querySelector("button.blue")
+const button_container = document.querySelector("aside")
+const color_picker = document.querySelector(".color-picker")
+const grid_size_slider = document.querySelector(".slider")
+
+//the grid size selector slider value
+const grid_size_display = document.createElement("div")
+grid_size_display.classList.add("size-selector")
+grid_size_display.classList.add("roboto-condensed")
+grid_size_display.textContent = String( grid_size_slider.value )
+button_container.appendChild(grid_size_display)
 
 // important variables
 let mousedown = 0;
-let color = "black"
+let style = "color"
 let size = 16
+let color = "black"
 
 // initial grid creation
 grid_creator()
@@ -29,26 +41,59 @@ function grid_creator() {
     }
 }
 
-// drawing and hover effect
+// hover effect enter logic and implementation functions
 function hovereffectenter(target_pixel) {
+    switch (style) {
+        case "color": {
+            target_pixel.style.opacity = 1;
+            color = color_picker.value
+            hoverenterlogic(target_pixel)
+            break
+        }
+        case "rainbow": {
+            target_pixel.style.opacity = 1;
+            let red = Math.floor(Math.random() * 255)
+            let blue = Math.floor(Math.random() * 255)
+            let green = Math.floor(Math.random() * 255)
+            color = `rgb(${red}, ${blue}, ${green})`
+            hoverenterlogic(target_pixel)
+            break
+        }
+        case "shading": {
+            color = color_picker.value
+            if (mousedown == 1 && target_pixel.style.backgroundColor == "") {
+                target_pixel.style.opacity = 0.1;
+            }
+            else if (mousedown) {
+                console.log("check")
+                target_pixel.style.opacity = String(Number(target_pixel.style.opacity) + 0.1)
+            }
+            hoverenterlogic(target_pixel)
+            break
+        }
+
+    }
+}
+function hoverenterlogic(target_pixel) {
     if (mousedown) {
         target_pixel.style.backgroundColor = color
     }
-    else if (target_pixel.style.backgroundColor != color) {
+    else if (target_pixel.style.backgroundColor == "")
         target_pixel.style.backgroundColor = "#eeeeee"
+}
+
+// hover effect leave logic and implementation functions
+function hovereffectleave(target_pixel) {
+    if (mousedown == 0 && target_pixel.style.backgroundColor == "rgb(238, 238, 238)") {
+        target_pixel.style.backgroundColor = ""
     }
 }
 
-function hovereffectleave(target_pixel) {
-    if (mousedown == 0 && target_pixel.style.backgroundColor != color) {
-        target_pixel.style.backgroundColor = "white"
-    }
-    else if (mousedown == 1 && target_pixel.style.backgroundColor == "#eeeeee") {
-        target_pixel.style.backgroundColor = "white"
-    }
-}
+// for making the first pixel on click colored
 function onclicki(target_pixel) {
     target_pixel.style.backgroundColor = color
+    if (style == "shading")
+        target_pixel.style.opacity = 0.1
 }
 
 //button function declarations
@@ -60,9 +105,22 @@ function clear() {
     grid_creator()
 }
 
+//slider change function     
+function slider_change() {
+    size = grid_size_slider.value
+    grid_size_display.textContent = grid_size_slider.value
+    clear()
+}
+
 // mousedown/up event listeners
 document.addEventListener("mousedown", () => mousedown++)
 document.addEventListener("mouseup", () => mousedown--)
 
 // button event listeners
 clear_button.addEventListener("click", clear)
+rainbow_button.addEventListener("click", () => { style = "rainbow" })
+color_button.addEventListener("click", () => { style = "color" })
+shading_button.addEventListener("click", () => { style = "shading" })
+
+//slider event listener
+grid_size_slider.addEventListener("change", slider_change)
